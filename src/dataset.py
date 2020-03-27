226ebd29f02b02a893209dacd9d2264635f6e57f
EdouardYvinec <edouardyvinec@hotmail.fr>
2020-03-27 14:51:01 +0100

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -6,14 +6,16 @@ def images_paths():
 	test_ground_truth = []
 	image_names = []
 
-	list_images = os.listdir('../dataset/DIV2K_train_HR/')
-	list_images.sort()
-	for elem in list_images:
-		if elem[0] != '.':
-			train_ground_truth.append('../dataset/DIV2K_train_HR/' + elem)
+	if 'DIV2K_train_HR' in os.listdir('../dataset/'):
+		list_images = os.listdir('../dataset/DIV2K_train_HR/')
+		list_images.sort()
+		for elem in list_images:
+			if elem[0] != '.':
+				train_ground_truth.append('../dataset/DIV2K_train_HR/' + elem)
 	list_images = os.listdir('../dataset/DIV2K_valid_HR/')
 	list_images.sort()
 	for elem in list_images:
-		test_ground_truth.append('../dataset/DIV2K_valid_HR/' + elem)
+		if elem[0] != '.':
+			test_ground_truth.append('../dataset/DIV2K_valid_HR/' + elem)
 
 	return train_ground_truth, test_ground_truth
